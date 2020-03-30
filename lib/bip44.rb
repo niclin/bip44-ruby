5333c5d3036f3e89e6650c6681f58cee521ef244
Nic <niclin0226@gmail.com>
2020-03-30 18:29:19 +0800

    Add some currency

--- lib/bip44.rb
+++ lib/bip44.rb
@@ -1,6 +1,18 @@
 require "bip44/version"
+require "bip44/constants"
 
 module Bip44
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def get_hexa(currency)
+      currency = currency.downcase
+
+      currency_info = CONSTANT.find { |c| c[:currency] == currency }
+
+      raise Error, "Can not found hexa from #{currency}" if currency_info.nil?
+
+      currency_info[:constant]
+    end
+  end
 end
---
+++ lib/bip44/constants.rb
@@ -0,0 +1,9 @@
+module Bip44
+  CONSTANT = [
+    { currency: "btc", name: "Bitcoin", constant: "0x80000000" },
+    { currency: "eth", name: "Ether", constant: "0x8000003c" },
+    { currency: "ltc", name: "Litecoin", constant: "0x80000002" },
+    { currency: "bch", name: "Bitcoin Cash", constant: "0x80000091" },
+    { currency: "xrp", name: "Ripple", constant: "0x80000090" }
+  ].freeze
+end
