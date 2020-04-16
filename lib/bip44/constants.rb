893cfe17775132dbdf9bc1b7dd86d73583e54cb3
Nic <niclin0226@gmail.com>
2020-04-16 12:48:33 +0800

    Bump version

--- lib/bip44/constants.rb
+++ lib/bip44/constants.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 module Bip44
-  CONSTANT = YAML.load(File.read("bip44.yml")).freeze
+  SETTINGS_PATH = File.join(File.dirname(__FILE__), 'bip44.yml')
+  CONSTANT = YAML.load(File.read(SETTINGS_PATH)).freeze
 end
